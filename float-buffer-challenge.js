9d256a7a25d6f5795215f453a4988e3db60850d9
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-03-27 17:02:06 -0400

    Float buffer solution

--- float-buffer-challenge.js
+++ float-buffer-challenge.js
@@ -2,11 +2,22 @@ const { Buffer } = require('node:buffer');
 const bufferToBinary = require('./buffer-to-binary');
 
 function writeFloatsBE(floats) {
+    const buffer = Buffer.alloc(floats.length * 4);
 
+    floats.forEach((float, i) => {
+        buffer.writeFloatBE(float, i * 4)
+    });
+
+    return buffer;
 }
 
 function readFloatsBE(buffer) {
-
+    const numberOfFloats = buffer.length / 4;
+    const floats = [];
+    for (let i = 0; i < numberOfFloats; i++) {
+        floats.push(buffer.readFloatBE(i * 4));
+    }
+    return floats;
 }
 
 const buffer = writeFloatsBE([1.5, 2.3, 100.18]);
