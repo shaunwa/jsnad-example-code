0d46b1cbb131740e03509a1c5215df0b5afcfd71
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-03-29 13:35:18 -0400

    Callbacks solution

--- callbacks-challenge.js
+++ callbacks-challenge.js
@@ -1,11 +1,42 @@
+const fs = require('fs');
+
 // Wait x seconds...
+setTimeout(() => {
+    console.log('Starting operations...');
 
-// Read the contents of some-text-file.txt
+    // Read the contents of some-text-file.txt
+    fs.readFile('some-text-file.txt', 'utf8', (err1, data1) => {
+        if (err1) {
+            console.error(err1);
+            return;
+        }
 
-// Read the contents of another-text-file.txt
+        // Read the contents of another-text-file.txt
+        fs.readFile('another-text-file.txt', 'utf8', (err2, data2) => {
+            if (err2) {
+                console.error(err2);
+                return;
+            }
 
-// Print out combined file contents
+            // Print out combined file contents
+            console.log(data1 + ' ' + data2);
 
-// Delete both files
+            // Delete both files
+            fs.unlink('some-text-file.txt', (err3) => {
+                fs.unlink('another-text-file.txt', (err4) => {
+                    if (err3) {
+                        console.log('Unable to delete some-text-file.txt');
+                    }
 
-// Print out "success!"
+                    if (err4) {
+                        console.log('Unable to delete another-text-file.txt');
+                    }
+
+                    console.log('Done!');
+                });
+            });
+        });
+
+    });
+
+}, 2000);
