d6d32644711a15df29693bd4ba6beb53591ba7ba
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-04-02 12:32:29 -0400

    Map async solution

--- async-map-challenge.js
+++ async-map-challenge.js
@@ -1,6 +1,12 @@
+const delay = function(ms, value) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(value), ms);
+    });
+}
 
 function mapAsync(arr, fn) {
-
+    return Promise.allSettled(arr.map(fn))
+        .then(results => results.map(r => r.value));
 }
 
 const urls = [
@@ -9,5 +15,7 @@ const urls = [
     'www.site-3.com/products',
 ];
 
-mapAsync(urls, url => axios.get(url))
-    .then((results) => { });
+mapAsync(urls, url => delay(1000, 'Successfully loaded data from ' + url))
+    .then((results) => {
+        console.log(results);
+    });
