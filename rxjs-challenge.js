77e522b2616198c88690a6313f8e988fa9cb678f
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-04-10 13:57:44 -0400

    RxJS Solution

--- rxjs-challenge.js
+++ rxjs-challenge.js
@@ -1,24 +1,31 @@
+const { Observable, map } = require('rxjs');
 const readline = require('readline');
 const fs = require('fs');
 
-const rl = readline.createInterface({
-    input: fs.createReadStream('rxjs-challenge.txt'),
-    crlfDelay: Infinity
-});
+function readLines(filePath) {
+    return new Observable(subscriber => {
+        const rl = readline.createInterface({
+            input: fs.createReadStream(filePath),
+            crlfDelay: Infinity
+        });
 
-rl.on('line', (line) => {
-    //
-});
+        rl.on('line', (line) => {
+            subscriber.next(line);
+        });
 
-rl.on('close', () => {
-    //
-});
+        rl.on('close', () => {
+            subscriber.complete();
+            rl.close();
+        });
 
-rl.on('error', (error) => {
-    //
-});
+        rl.on('error', (error) => {
+            subscriber.error(error);
+            rl.close();
+        });
+    });
+}
 
-readLines('rxjs-challenge.txt').subscribe({
+readLines('rxjs-challenge.txt').pipe(map(x => x.toUpperCase())).subscribe({
   next: line => console.log('Read line:', line),
   error: err => console.error('Error reading file:', err),
   complete: () => console.log('Finished reading file')
