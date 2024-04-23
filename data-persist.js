db3c8c52b47285cacf5918cc52df81b4142df9dd
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-04-23 10:44:15 -0400

    Data persistence solution

--- data-persist.js
+++ data-persist.js
@@ -1,11 +1,27 @@
 const readline = require('readline');
+const fs = require('fs');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-rl.question('Name: ', (name) => {
+fs.promises.readFile('people-persist.txt', 'utf8')
+    .then(content => {
+        const peopleStrings = content.split('\n');
+        const people = peopleStrings.filter(x => x.includes('{')).map(ps => JSON.parse(ps));
+        console.log(people);
 
-    rl.close();
-});
+        rl.question('Name: ', (name) => {
+            rl.question('Age: ', (age) => {
+                rl.question('Hair Color: ', (hairColor) => {
+                    const person = { name, age, hairColor };
+                    fs.promises.appendFile('people-persist.txt', JSON.stringify(person) + '\n')
+                        .then(() => {
+                            console.log('Success!');
+                            rl.close();
+                        });
+                });
+            });
+        });
+    });
